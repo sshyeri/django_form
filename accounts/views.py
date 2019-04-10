295038b9755a4d7157372f62ddbcb362fd5caeee
sshyeri <sshyeri.k@gmail.com>
2019-04-10 01:40:59 +0000

    09_Gravatar

--- accounts/views.py
+++ accounts/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth import update_session_auth_hash
-from .forms import UserCustomChangeForm
+from .forms import UserCustomChangeForm, UserCustomCreationForm
 
 # Create your views here.
 def signup(request):
     if request.user.is_authenticated:
         return redirect('boards:index')
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserCustomCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
             return redirect('boards:index')
     else:
-        form = UserCreationForm
+        form = UserCustomCreationForm
     context = {'form': form}
     return render(request, 'accounts/auth_form.html', context)
 
